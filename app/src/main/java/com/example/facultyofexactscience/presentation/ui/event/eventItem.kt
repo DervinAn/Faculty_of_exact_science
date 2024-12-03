ede6fa6ac9be743818ec9dd3827897a0704fd14f
Anas_Dervin <102409151+Anas040@users.noreply.github.com>
2024-12-03 21:32:24 +0100

    G

--- app/src/main/java/com/example/facultyofexactscience/presentation/ui/event/eventItem.kt
+++ app/src/main/java/com/example/facultyofexactscience/presentation/ui/event/eventItem.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,7 +26,6 @@ import androidx.tv.material3.CardDefaults
 import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.ShapeDefaults
 import androidx.tv.material3.Text
-import com.example.facultyofexactscience.Event
 import com.example.facultyofexactscience.ui.theme.border
 import com.example.facultyofexactscience.ui.theme.containerColor
 
